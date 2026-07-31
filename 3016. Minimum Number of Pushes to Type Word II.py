7be541dc19addf383a3712976d569f51d3f13fff
Durgesh83kumar <durgeshkumar83928137@gmail.com>
2026-07-31 19:22:18 +0530

    3016. Minimum Number of Pushes to Type Word II

---
+++ 3016. Minimum Number of Pushes to Type Word II.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        freq = Counter(word)
+
+        # Frequencies in descending order
+        counts = sorted(freq.values(), reverse=True)
+
+        ans = 0
+
+        for i, f in enumerate(counts):
+            pushes = i // 8 + 1
+            ans += f * pushes
+
+        return ans
