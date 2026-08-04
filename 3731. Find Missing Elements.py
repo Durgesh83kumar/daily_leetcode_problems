e97b6c1b4102a3592ce0b95c49eb6974cdd7c74a
Durgesh83kumar <durgeshkumar83928137@gmail.com>
2026-08-04 11:03:08 +0530

    3731. Find Missing Elements

---
+++ 3731. Find Missing Elements.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        min_num = min(nums)
+        max_num = max(nums)
+
+        num_set = set(nums)
+        ans = []
+
+        for num in range(min_num, max_num + 1):
+            if num not in num_set:
+                ans.append(num)
+
+        return ans
