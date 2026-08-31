99dbf6d7ce7335d8aae7237a039c3e8d64ef29b0
Durgesh83kumar <durgeshkumar83928137@gmail.com>
2026-08-31 22:25:09 +0530

    2058. Find the Minimum and Maximum Number of Nodes Between Critical Points

---
+++ 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def nodesBetweenCriticalPoints(self, head):
+        prev = head
+        curr = head.next
+        pos = 1
+
+        first = -1
+        last = -1
+        min_dist = float('inf')
+
+        while curr.next:
+            next_node = curr.next
+
+            if ((curr.val > prev.val and curr.val > next_node.val) or
+                (curr.val < prev.val and curr.val < next_node.val)):
+
+                if first == -1:
+                    first = pos
+                else:
+                    min_dist = min(min_dist, pos - last)
+
+                last = pos
+
+            prev = curr
+            curr = next_node
+            pos += 1
+
+        if first == -1 or first == last:
+            return [-1, -1]
+
+        return [min_dist, last - first]
