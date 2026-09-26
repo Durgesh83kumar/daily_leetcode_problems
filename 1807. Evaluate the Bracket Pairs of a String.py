b725c897f4a29effd3304f82b023bdce8766eb83
Durgesh83kumar <durgeshkumar83928137@gmail.com>
2026-09-26 16:46:24 +0530

    1807. Evaluate the Bracket Pairs of a String

---
+++ 1807. Evaluate the Bracket Pairs of a String.py
@@ -0,0 +1,39 @@
+class Solution(object):
+
+    def evaluate(self, s, knowledge):
+        """
+        :type s: str
+        :type knowledge: List[List[str]]
+        :rtype: str
+        """
+        mp = {}
+
+        for key, value in knowledge:
+            mp[key] = value
+
+        ans = []
+        i = 0
+
+        while i < len(s):
+
+            if s[i] == '(':
+
+                j = i + 1
+
+                while s[j] != ')':
+                    j += 1
+
+                key = s[i + 1:j]
+
+                if key in mp:
+                    ans.append(mp[key])
+                else:
+                    ans.append("?")
+
+                i = j + 1
+
+            else:
+                ans.append(s[i])
+                i += 1
+
+        return ''.join(ans)
